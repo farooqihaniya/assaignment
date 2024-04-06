20c242537d6617b21784bef755e344648233df5f
farooqihaniya <165930336+farooqihaniya@users.noreply.github.com>
2024-04-06 21:26:28 +0530

    Update TestMultiPriority.java

--- TestMultiPriority.java
+++ TestMultiPriority.java
@@ -3,16 +3,26 @@ public class TestMultiPriority extends Thread
 {    
     public void run()  
     {    
+	    System.out.println("Thread name: "+Thread.currentThread().getName());
         System.out.println("Priority of thread is: "+Thread.currentThread().getPriority());    
+    }
     }    
+public class Main{
+	
     public static void main(String args[])  
     {    
-        // creating one thread   
-        TestMultiPriority1 t1=new TestMultiPriority1();   
-		TestMultiPriority1 t2=new TestMultiPriority1();
-        // print the maximum priority of this thread  
-        t1.setPriority(Thread.MAX_PRIORITY); 
-         t2.setPriority(Thread.MIN_PRIORITY); 		
+        // creating  thread   
+        TestMultiPriority1 m1=new TestMultiPriority1();   
+		TestMultiPriority1 m2=new TestMultiPriority1();
+	    m1.setName("Mythread-1");
+	    m2.setName("Mythread-2");
+         
+        m1.setPriority(Thread.MIN_PRIORITY); 
+         m2.setPriority(Thread.MAX_PRIORITY); 
+	    m1.start();
+	    m2.start();
+    }
+}
         // call the run() method  
          t1.start(); 
 		 t2.start(); 
